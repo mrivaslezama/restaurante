f8015156a49cc61d5f1d8f779242be82e8eee735
Miguel Rivas <mrivaslezama@gmail.com>
2023-05-22 16:32:28 -0400

    new page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import './app.css'
 import App from './App.svelte'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
 
 const app = new App({
   target: document.getElementById('app'),
 })
 
 export default app
+
+
