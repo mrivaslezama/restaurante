abcd2524bbd6302bb25910b8f9714bc254929bfc
Miguel Rivas <mrivaslezama@gmail.com>
2023-07-31 22:10:58 -0400

    creando restaurante

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Templeton from './components/Templeton.vue'
+import Navbar from './components/Navbar.vue'
+import Cards from './components/Cards.vue'
+import Jumbotron from './components/Jumbotron.vue'
+</script>
+
+<template>
+  <Navbar/>
+  <Jumbotron/>
+ 
+  <Templeton/>
+  <Cards/>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,28 @@
+<template>
+
+<div class="flex items-center justify-center py-4 md:py-8 flex-wrap">
+    <h1>Galeria</h1>
+
+</div>
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="/src/assets/sopa.png" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="/src/assets/reineton.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="/src/assets/pabell2.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="/src/assets/milanesa.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="/src/assets/mecha2.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="/src/assets/completos2.jpg" alt="">
+    </div>
+</div>
+
+</template>
